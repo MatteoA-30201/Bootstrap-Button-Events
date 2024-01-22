3e65a3c82387c9190e45244024acc5c5c3577a26
Matteo <125mamatucci@frhsd.com>
2024-01-22 10:58:33 -0500

    commit

---
+++ js/mouseEvents.js
@@ -0,0 +1,7 @@
+let btnOne = document.getElementById("click");
+function makeGreen() {
+  // #a0b96b
+  btnOne.style.background = "#a0b96b";
+}
+document.getElementById("click");
+btnOne.addEventListener("click", makeGreen);
